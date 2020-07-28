a1341efdcdbf63e4b1471406203dd738292d03a6
Paul Gaumer <mitsudomoe8@gmail.com>
2020-07-28 15:39:01 +0200

    Add container to layout. Add Tailwind UI, custom font & colors to tailwind config

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Image from '../components/image';
 import SEO from '../components/seo';
 
 const Button = styled.button`
-  ${tw`bg-blue-500 hover:bg-blue-800 text-white p-2 rounded`}
+  ${tw`bg-primary hover:bg-blue-800 text-white p-2 rounded`}
 `;
 
 const IndexPage = () => (
@@ -16,12 +16,11 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <Button>Activate</Button>
     <p className="">Welcome to your new Gatsby site.</p>
-    <p className="text-red-800">Now go build something great.</p>
+    <p className="text-secondary">Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ['Arial', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#33C8A3',
+        secondary: '#FDBF37',
+        edu: '#E8EBF7',
+        featured: '#EF4F5F',
+      },
+    },
+  },
   variants: {},
-  plugins: [],
+  plugins: [require('@tailwindcss/ui')],
 };
