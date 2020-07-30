12a24e9360b2862eaa42d16c6c8f961473b1a6c4
Paul Gaumer <mitsudomoe8@gmail.com>
2020-07-30 15:30:23 +0200

    Add background filter

--- src/components/sections/homepage/regionMap.js
+++ src/components/sections/homepage/regionMap.js
@@ -4,6 +4,8 @@ import { MdPlayArrow } from 'react-icons/md';
 import PdlMap from './vectorMap';
 import * as THREE from 'three';
 import NET from 'vanta/dist/vanta.net.min';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import SectionTitle from '../../sectionTitle';
 
 class RegionMap extends React.Component {
@@ -40,35 +42,57 @@ class RegionMap extends React.Component {
       { name: 'vendée', link: '/vendee' },
     ];
 
+    const LinksList = styled.ul`
+      background: rgba(255, 255, 255, 0.9);
+      box-shadow: 6px 6px 15px rgba(255, 255, 255, 0.3);
+      ${tw`flex flex-col space-y-2 py-4 px-4 rounded-md mt-16`}
+    `;
+
     return (
-      <div style={{ height: '800px' }} ref={this.vantaRef}>
-        <section className="text-white pt-20 relative">
-          <div className="flex flex-col items-center">
-            <SectionTitle text="Programme par département" color="white" />
-          </div>
-          <div id="map" className="flex space-x-10 justify-center -mt-10">
-            {/* Display clickable map */}
-            <PdlMap background="map" backgroundHover="mapLink" width="w-1/2" />
-            {/* Display departments list */}
-            <div className=" flex flex-col">
-              <ul className="flex flex-col space-y-2 bg-white py-4 px-4 rounded-md mt-16">
-                {departments.map((department) => (
-                  <li key={department.name}>
-                    <Link
-                      to={department.link}
-                      className="capitalize text-white bg-mapLink rounded-full py-2 px-4 inline-flex items-center space-x-1 hover:bg-mapLinkHover"
-                    >
-                      <MdPlayArrow className="text-2xl" />
-                      <span>{department.name}</span>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+      <>
+        <div
+          data-name="vanta wrapper"
+          style={{ height: '800px' }}
+          ref={this.vantaRef}
+        >
+          <section className="text-white pt-20 relative z-20">
+            <div className="flex flex-col items-center">
+              <SectionTitle text="Programme par département" color="white" />
             </div>
-          </div>
-          <div id="carousel"></div>
-        </section>
-      </div>
+            <div id="map" className="flex space-x-10 justify-center -mt-10">
+              {/* Display clickable map */}
+              <PdlMap
+                background="map"
+                backgroundHover="mapLink"
+                width="w-1/2"
+              />
+              {/* Display departments list */}
+              <div className=" flex flex-col">
+                <LinksList>
+                  {departments.map((department) => (
+                    <li key={department.name}>
+                      <Link
+                        to={department.link}
+                        className="capitalize text-white bg-mapLink rounded-full py-2 px-4 inline-flex items-center space-x-1 hover:bg-mapLinkHover"
+                      >
+                        <MdPlayArrow className="text-2xl" />
+                        <span>{department.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </LinksList>
+              </div>
+            </div>
+            <div id="carousel"></div>
+          </section>
+          {/* BACKGROUND FILTER */}
+          <div
+            className="absolute inset-0 z-10"
+            style={{ background: '#000D30', opacity: 0.2 }}
+          ></div>
+          {/* ****************** */}
+        </div>
+      </>
     );
   }
 }
