d67075738fdf6826b74e71b03c18e3ba18b6d39b
Paul Gaumer <mitsudomoe8@gmail.com>
2020-07-30 11:52:53 +0200

    Add vanta

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -8,6 +8,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
 
 import Header from '../header/header';
 import './layout.css';
@@ -25,6 +26,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet defer={false}>
+        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/96/three.min.js"></script> */}
+        {/* <script src="https://cdn.jsdelivr.net/gh/tengbao/vanta@master/dist/vanta.net.min.js"></script> */}
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-sectionBackground">
         <main>{children}</main>
--- src/components/sections/homepage/regionMap.js
+++ src/components/sections/homepage/regionMap.js
@@ -3,46 +3,97 @@ import { Link } from 'gatsby';
 import { MdPlayArrow } from 'react-icons/md';
 import SectionTitle from '../../sectionTitle';
 import PdlMap from './vectorMap';
+import { render } from 'react-dom';
+import NET from 'vanta/dist/vanta.net.min';
 
-const RegionMap = () => {
-  const departments = [
-    { name: 'loire atlantique', link: '/loire-atlantique' },
-    { name: 'maine et loire', link: '/maine-et-loire' },
-    { name: 'mayenne', link: '/mayenne' },
-    { name: 'sarthe', link: '/sarthe' },
-    { name: 'vendée', link: '/vendee' },
-  ];
-  return (
-    <section
-      className="text-white py-20"
-      style={{ backgroundColor: '#1B133A' }}
-    >
-      <div className="flex flex-col items-center">
-        <SectionTitle text="Programme par département" color="white" />
+class RegionMap extends React.Component {
+  constructor() {
+    super();
+    this.vantaRef = React.createRef();
+  }
+  componentDidMount() {
+    this.vantaEffect = NET({
+      el: this.vantaRef.current,
+      color: '#FF367F',
+      backgroundColor: '#FDBF37',
+      points: 10,
+      maxDistance: 20,
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+    });
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy();
+  }
+
+  render() {
+    // const departments = [
+    //   { name: 'loire atlantique', link: '/loire-atlantique' },
+    //   { name: 'maine et loire', link: '/maine-et-loire' },
+    //   { name: 'mayenne', link: '/mayenne' },
+    //   { name: 'sarthe', link: '/sarthe' },
+    //   { name: 'vendée', link: '/vendee' },
+    // ];
+    return (
+      <div style={{ height: '922px' }} ref={this.vantaRef}>
+        Foreground content goes here
       </div>
-      <div id="map" className="flex space-x-10 justify-center">
-        {/* Display clickable map */}
-        <PdlMap background="map" backgroundHover="mapLink" width="w-1/2" />
-        {/* Display departments list */}
-        <div className=" flex flex-col">
-          <ul className="flex flex-col space-y-2 bg-white py-4 px-4 rounded-md mt-16">
-            {departments.map((department) => (
-              <li key={department.name}>
-                <Link
-                  to={department.link}
-                  className="capitalize text-white bg-mapLink rounded-full py-2 px-4 inline-flex items-center space-x-1 hover:bg-mapLinkHover"
-                >
-                  <MdPlayArrow className="text-2xl" />
-                  <span>{department.name}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div id="carousel"></div>
-    </section>
-  );
-};
+      // <div style={{ height: '922px' }} ref={this.vantaRef}>
+      //   <section id="banner" className={'banner' ? 'minor' : 'major fit'}>
+      //     <div className="inner text-white">
+      //       <header className="major">
+      //         {/* <h1 style={{ textAlign: 'right' }}></h1> */}
+      //       </header>
+      //       <p style={{ float: 'right' }}>
+      //         {
+      //           <React.Fragment>
+      //             Luminosity is an interdisciplinary research and development
+      //             lab driven by a hand-selected team of high-performing students
+      //             from Arizona State University. <br />
+      //             <br />
+      //             We design, build, and deploy novel solutions to address some
+      //             of the world’s most pressing challenges.
+      //           </React.Fragment>
+      //         }
+      //       </p>
+      //     </div>
+      //   </section>
+      // </div>
+      // <section
+      //   className="text-white py-20"
+      //   style={{ backgroundColor: '#1B133A' }}
+      // >
+      //   <div className="flex flex-col items-center">
+      //     <SectionTitle text="Programme par département" color="white" />
+      //   </div>
+      //   <div id="map" className="flex space-x-10 justify-center">
+      //     {/* Display clickable map */}
+      //     <PdlMap background="map" backgroundHover="mapLink" width="w-1/2" />
+      //     {/* Display departments list */}
+      //     <div className=" flex flex-col">
+      //       <ul className="flex flex-col space-y-2 bg-white py-4 px-4 rounded-md mt-16">
+      //         {departments.map((department) => (
+      //           <li key={department.name}>
+      //             <Link
+      //               to={department.link}
+      //               className="capitalize text-white bg-mapLink rounded-full py-2 px-4 inline-flex items-center space-x-1 hover:bg-mapLinkHover"
+      //             >
+      //               <MdPlayArrow className="text-2xl" />
+      //               <span>{department.name}</span>
+      //             </Link>
+      //           </li>
+      //         ))}
+      //       </ul>
+      //     </div>
+      //   </div>
+      //   <div id="carousel"></div>
+      // </section>
+    );
+  }
+}
 
 export default RegionMap;
