68940c0e339cc4447f3cbbcf8436e205ba0b8975
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-24 23:03:50 +0200

    wip: clg debugging netlify function

--- functions/instagramByHashtag/instagramByHashtag.js
+++ functions/instagramByHashtag/instagramByHashtag.js
@@ -12,14 +12,16 @@ const cache = {
 
 async function getPosts() {
   // first see if we have a cache in 30 min
-  const timeSinceLastFetch = Date.now() - cache.lastFetch;
-  if (timeSinceLastFetch <= 600000) {
-    return cache.posts;
-  }
+  // const timeSinceLastFetch = Date.now() - cache.lastFetch;
+  // if (timeSinceLastFetch <= 600000) {
+  //   return cache.posts;
+  // }
+  console.log('YOO FROM GETPOSTS');
+  console.log(url);
   const data = await fetch(url).then((res) => res.json());
   const posts = slimUpPosts(data);
-  cache.lastFetch = Date.now();
-  cache.posts = posts;
+  // cache.lastFetch = Date.now();
+  // cache.posts = posts;
   return posts;
 }
 
