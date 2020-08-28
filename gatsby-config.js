19d480c7327495cda551b63b46141742d5f66b0a
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-28 18:07:04 +0200

    feature: add sitemap and robot-txt plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
+// Needed to disable robots-txt crawlers for Netlify deploy-previews
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://www.fetedelascience-paysdelaloire.fr',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title: `Fête de la Science`,
     description: `Fête de la Science 2020 des Pays de la Loire`,
+    siteUrl: siteUrl,
     author: `@paulgaumer`,
   },
   plugins: [
@@ -39,6 +50,29 @@ module.exports = {
         },
         // Defines the environments where the tracking should be available  - default is ["production"]
         environments: ['production', 'development'],
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      // Needed to disable robots-txt crawlers for Netlify deploy-previews
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
       },
     },
     {
--- src/components/layout/seo.js
+++ src/components/layout/seo.js
@@ -5,10 +5,10 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -23,9 +23,9 @@ function SEO({ description, lang, meta, title }) {
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
@@ -67,22 +67,27 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: 'keywords',
+          content:
+            'science, Pays de la Loire, événement, conférence, éducation, ateliers',
+        },
       ].concat(meta)}
     />
-  )
+  );
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: `fr`,
   meta: [],
   description: ``,
-}
+};
 
 SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
-}
+};
 
-export default SEO
+export default SEO;
