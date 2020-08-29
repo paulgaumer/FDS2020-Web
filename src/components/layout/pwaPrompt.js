1dc83bf535e92bd88d92a06334a9a51e0a0aff68
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-29 21:06:23 +0200

    fix: ssr error on pwa prompt

--- src/components/layout/pwaPrompt.js
+++ src/components/layout/pwaPrompt.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import PWAPrompt from 'react-ios-pwa-prompt';
 import { hasWindow } from '../../utils/hasWindow';
+// Need to require conditionally to avoid a missing "document" error during SSR
+const PWAPrompt = hasWindow ? require('react-ios-pwa-prompt') : null;
 
 const IosPwaPrompt = () => {
   const [isWindowLoaded, setIsWindowLoaded] = useState(false);
@@ -11,19 +12,21 @@ const IosPwaPrompt = () => {
     }
   }, []);
 
-  return isWindowLoaded ? (
-    <PWAPrompt
-      delay={20000}
-      copyTitle="Télécharger l'app"
-      copyBody="Ce site peut être utilisé comme une app. Ajoutez le à votre écran d'accueil pour l'utiliser en modes plein écran et hors-ligne."
-      copyShareButtonLabel="1) Appuyez sur l'icône 'Partager'"
-      copyAddHomeButtonLabel="2) Appuyez sur 'Sur l'écran d'accueil'"
-      copyClosePrompt="Fermer"
-      debug={true}
-    />
-  ) : (
-    <div className="hidden" />
-  );
+  if (isWindowLoaded) {
+    return (
+      <PWAPrompt
+        delay={20000}
+        copyTitle="Télécharger l'app"
+        copyBody="Ce site peut être utilisé comme une app. Ajoutez le à votre écran d'accueil pour l'utiliser en mode plein écran et hors-ligne."
+        copyShareButtonLabel="1) Appuyez sur l'icône 'Partager'"
+        copyAddHomeButtonLabel="2) Appuyez sur 'Sur l'écran d'accueil'"
+        copyClosePrompt="Fermer"
+        // debug={true}
+      />
+    );
+  } else {
+    return <div className="hidden" />;
+  }
 };
 
 export default IosPwaPrompt;
