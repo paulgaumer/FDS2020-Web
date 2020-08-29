e621780ff1b99ab571c00e4f564c0760f1ea0fdb
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-29 18:31:59 +0200

    feature: add PWA prompt for iOS

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,6 +4,7 @@ import './layout.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import CookieBanner from './cookieBanner';
+import PWAPrompt from 'react-ios-pwa-prompt';
 import HideNavOnScrollHook from '../../utils/scrollNavHook';
 
 const Layout = ({ children, headerHidden }) => {
@@ -24,6 +25,15 @@ const Layout = ({ children, headerHidden }) => {
         <Footer />
       </div>
       <CookieBanner />
+      <PWAPrompt
+        delay={20000}
+        copyTitle="Télécharger l'app"
+        copyBody="Ce site peut être utilisé comme une app. Ajoutez le à votre écran d'accueil pour l'utiliser en modes plein écran et hors-ligne."
+        copyShareButtonLabel="1) Appuyez sur l'icône 'Partager'"
+        copyAddHomeButtonLabel="2) Appuyez sur 'Sur l'écran d'accueil'"
+        copyClosePrompt="Fermer"
+        // debug={true}
+      />
     </div>
   );
 };
