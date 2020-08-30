a8ed91ac628c95bdbdfa2d86a0d746023308af83
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-30 19:07:53 +0200

    fix: build error

--- src/components/layout/pwaPrompt.js
+++ src/components/layout/pwaPrompt.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { hasWindow } from '../../utils/hasWindow';
 // Need to require conditionally to avoid a missing "document" error during SSR
-const PWAPrompt = hasWindow ? require('react-ios-pwa-prompt') : null;
+const PWAPrompt = React.lazy(() => import('react-ios-pwa-prompt'));
 
 const IosPwaPrompt = () => {
   const [isWindowLoaded, setIsWindowLoaded] = useState(false);
@@ -14,15 +14,17 @@ const IosPwaPrompt = () => {
 
   if (isWindowLoaded) {
     return (
-      <PWAPrompt
-        delay={20000}
-        copyTitle="Télécharger l'app"
-        copyBody="Ce site peut être utilisé comme une app. Ajoutez le à votre écran d'accueil pour l'utiliser en mode plein écran et hors-ligne."
-        copyShareButtonLabel="1) Appuyez sur l'icône 'Partager'"
-        copyAddHomeButtonLabel="2) Appuyez sur 'Sur l'écran d'accueil'"
-        copyClosePrompt="Fermer"
-        // debug={true}
-      />
+      <Suspense fallback={null}>
+        <PWAPrompt
+          delay={20000}
+          copyTitle="Télécharger l'app"
+          copyBody="Ce site peut être utilisé comme une app. Ajoutez le à votre écran d'accueil pour l'utiliser en mode plein écran et hors-ligne."
+          copyShareButtonLabel="1) Appuyez sur l'icône 'Partager'"
+          copyAddHomeButtonLabel="2) Appuyez sur 'Sur l'écran d'accueil'"
+          copyClosePrompt="Fermer"
+          // debug={true}
+        />
+      </Suspense>
     );
   } else {
     return <div className="hidden" />;
