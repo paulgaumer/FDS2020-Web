4045fb448f5fc334ff76bb9edff6513863f2b8a6
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-30 18:29:23 +0200

    fix: carousel height

--- src/components/sections/editoPage/carouselSection.js
+++ src/components/sections/editoPage/carouselSection.js
@@ -6,34 +6,69 @@ import SectionTitle from '../../global/sectionTitle';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const PicContainer = styled.div``;
+const PicsContainer = styled.div`
+  height: 300px;
+
+  .carousel-wrapper,
+  .carousel,
+  .carousel > .slider-wrapper,
+  .carousel > .slider-wrapper > .slider {
+    height: 100%;
+  }
+
+  .image-container {
+    height: 100%;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+  @media (min-width: 768px) {
+    height: 400px;
+  }
+  @media (min-width: 1024px) {
+    height: 600px;
+  }
+`;
 
 const CarouselSection = ({ previousEditions }) => {
   return (
     <SectionWrapper>
       <SectionContainer customClasses="py-16 md:py-20">
         <SectionTitle text="Editions Précedentes" />
-        <Carousel
-          autoPlay
-          infiniteLoop
-          interval={3000}
-          showStatus={false}
-          swipeable={true}
-          showThumbs={true}
-          dynamicHeight={true}
-        >
-          {previousEditions.map((pic) => {
-            return (
-              <PicContainer key={pic.asset.id}>
-                <img
-                  src={pic.asset.url}
-                  alt={pic.alt ? pic.alt : 'Edition précédente'}
-                />
-                {pic.caption && <p className="legend">{pic.caption}</p>}
-              </PicContainer>
-            );
-          })}
-        </Carousel>
+        <PicsContainer>
+          <Carousel
+            autoPlay
+            infiniteLoop
+            interval={3000}
+            showStatus={false}
+            swipeable={true}
+            showThumbs={true}
+            // dynamicHeight={true}
+            className="carousel-wrapper"
+          >
+            {previousEditions.map((pic) => {
+              return (
+                // <PicContainer key={pic.asset.id}>
+                //   <img
+                //     src={pic.asset.url}
+                //     alt={pic.alt ? pic.alt : 'Edition précédente'}
+                //   />
+                //   {pic.caption && <p className="legend">{pic.caption}</p>}
+                // </PicContainer>
+
+                <div
+                  className="image-container"
+                  style={{
+                    backgroundImage: `url(${pic.asset.url})`,
+                  }}
+                >
+                  {pic.caption && <p className="legend">{pic.caption}</p>}
+                </div>
+              );
+            })}
+          </Carousel>
+        </PicsContainer>
       </SectionContainer>
     </SectionWrapper>
   );
