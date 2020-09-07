065207e382d94ec08d5e02f032936bf3e6dca09d
Paul Gaumer <mitsudomoe8@gmail.com>
2020-09-07 10:05:16 +0200

    fix: change max width on logos grid

--- src/components/sections/eventsIndex/localPartnersSection.js
+++ src/components/sections/eventsIndex/localPartnersSection.js
@@ -3,7 +3,10 @@ import SectionContainer from '../../layout/sectionContainer';
 import SectionTitle from '../../global/sectionTitle';
 import LogosGrid from '../../global/logosGrid';
 
-const LocalPartnersSection = ({ logos, partnersTitle = '' }) => {
+const LocalPartnersSection = ({
+  logos,
+  partnersTitle = 'Nos Partenaires Locaux',
+}) => {
   return (
     <SectionContainer customClasses="pt-20 pb-40">
       <SectionTitle text={partnersTitle} />
