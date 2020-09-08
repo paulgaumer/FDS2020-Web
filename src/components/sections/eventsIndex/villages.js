c896e394a30f95b44ba2f18bfc7c94573d80d0f4
Paul Gaumer <mitsudomoe8@gmail.com>
2020-09-08 18:29:17 +0200

    feature add animated chevron below village section

--- src/components/sections/eventsIndex/villages.js
+++ src/components/sections/eventsIndex/villages.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import { FaCampground } from 'react-icons/fa';
+import styled from 'styled-components';
 import SectionContainer from '../../layout/sectionContainer';
 import VillageCard from './villageCard';
+
+const Separator = styled.div`
+  div {
+    display: grid;
+    grid-template-columns: minmax(50px, 1fr) auto minmax(50px, 1fr);
+    align-items: center;
+    text-align: center;
+    grid-gap: 30px;
+    width: 100%;
+  }
+
+  /* @media (min-width: 768px) { */
+  div:before,
+  div:after {
+    content: '';
+    border-top: 2px solid;
+  }
+  /* } */
+
+  .pulsing-chevron {
+    animation: bounce 4s infinite;
+
+    @keyframes bounce {
+      0% {
+        transform: translateY(0%);
+      }
+      50% {
+        transform: translateY(0%);
+      }
+      75% {
+        transform: translateY(-25%);
+        animationtimingfunction: cubic-bezier(0.8, 0, 1, 1);
+      }
+      100% {
+        transform: translateY(0);
+        animationtimingfunction: cubic-bezier(0, 0, 0.2, 1);
+      }
+    }
+  }
+`;
 
 const Villages = ({
   villages = [],
@@ -32,11 +73,26 @@ const Villages = ({
               );
             })}
         </div>
-        <hr
-          className={`${
-            villages.length >= 1 ? 'mt-16' : ''
-          } text-center text-gray-500 border-t-2 mx-36`}
-        />
+        <Separator
+          className={`mx-10 md:mx-20 ${villages.length >= 1 ? 'mt-16' : ''}`}
+        >
+          <div className="text-gray-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-8 h-8 pulsing-chevron"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+        </Separator>
       </div>
     </SectionContainer>
   );
