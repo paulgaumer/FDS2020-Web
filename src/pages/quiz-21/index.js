5f1e4beeac24a349b68175e03e970d0e0091c70e
Paul Gaumer <mitsudomoe8@gmail.com>
2021-09-09 12:00:23 +0200

    add sponsors to quiz index

--- src/pages/quiz-21/index.js
+++ src/pages/quiz-21/index.js
@@ -6,11 +6,18 @@ import Layout from '../../components/layout/layout';
 import SectionWrapper from '../../components/layout/sectionWrapper';
 import SectionContainer from '../../components/layout/sectionContainer';
 import SectionTitle from '../../components/global/sectionTitle';
+import LogosGrid from '../../components/global/logosGrid';
 import PortableText from '@sanity/block-content-to-react';
 import { serializers } from '../../utils/portableTextSerializers';
 
 const Quiz21Welcome = ({ data }) => {
-  const { sectionTitle, _rawWelcomeText, startButton } = data.sanityQuiz2021;
+  const {
+    sectionTitle,
+    _rawWelcomeText,
+    startButton,
+    titleSponsors,
+    logosSponsors,
+  } = data.sanityQuiz2021;
 
   return (
     <Layout>
@@ -22,10 +29,22 @@ const Quiz21Welcome = ({ data }) => {
             <PortableText blocks={_rawWelcomeText} serializers={serializers} />
             <Link
               to="/quiz-21/1"
-              className="px-4 py-2 mt-6 text-base font-bold leading-6 text-gray-700 uppercase transition duration-150 ease-in-out transform border border-transparent rounded-md cursor-pointer bg-primary hover:scale-105"
+              className="px-4 py-2 mt-6 text-lg font-bold leading-6 text-gray-700 uppercase transition duration-150 ease-in-out transform border border-transparent rounded-md cursor-pointer bg-primary hover:scale-105"
             >
               {startButton}
             </Link>
+            {logosSponsors?.length > 0 && (
+              <div className="flex flex-col self-start w-full mt-24 sponsors">
+                {titleSponsors && (
+                  <h3 className="self-start my-10 mt-0 mb-10 text-2xl text-gray-700 border-b-2 border-secondary">
+                    {titleSponsors}
+                  </h3>
+                )}
+                <div className="self-center w-2/3">
+                  <LogosGrid logos={logosSponsors} />
+                </div>
+              </div>
+            )}
           </div>
         </SectionContainer>
       </SectionWrapper>
@@ -41,6 +60,17 @@ export const query = graphql`
       sectionTitle
       _rawWelcomeText
       startButton
+      titleSponsors
+      logosSponsors {
+        image {
+          asset {
+            id
+            fluid(maxWidth: 500) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
     }
   }
 `;
